aa60d8fd45c88efd915e3b93ca4f7c4a6e8d96bc
Yashwanth Reddy <yashwanth.yenugu@gmail.com>
2025-07-17 06:28:17 +0000

    Add cron trigger

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,55 @@
 interface Env {
-	API_DATA: KVNamespace;
+  API_DATA: KVNamespace;
 }
 
 interface ApiData {
-	response: any;
-	responseTimeMs: number;
-	timestamp: string;
+  response: any;
+  responseTimeMs: number;
+  timestamp: string;
 }
 
 export default {
-	async scheduled(
-		controller: ScheduledController,
-		env: Env,
-		ctx: ExecutionContext,
-	) {
-		// Run every 15 minutes
-		if (controller.cron === "*/1 * * * *") {
-			const url = "https://ai-algo-om07.onrender.com/";
-			const start = Date.now();
-			let response, responseBody;
-			try {
-				response = await fetch(url);
-				responseBody = await response.text();
-			} catch (err) {
-				responseBody = `Error: ${err}`;
-			}
-			const end = Date.now();
-			const apiData: ApiData = {
-				response: responseBody,
-				responseTimeMs: end - start,
-				timestamp: new Date().toISOString(),
-			};
-			await env.API_DATA.put('last-api-data', JSON.stringify(apiData));
-			console.log("API called, data stored", apiData);
-		}
-	},
+  async scheduled(
+    controller: ScheduledController,
+    env: Env,
+    ctx: ExecutionContext
+  ) {
+    const url = "https://ai-algo-om07.onrender.com/";
+    const start = Date.now();
+    let response, responseBody;
+    try {
+      response = await fetch(url);
+      responseBody = await response.text();
+    } catch (err) {
+      responseBody = `Error: ${err}`;
+    }
+    const end = Date.now();
+    const apiData: ApiData = {
+      response: responseBody,
+      responseTimeMs: end - start,
+      timestamp: new Date().toISOString(),
+    };
+    await env.API_DATA.put("last-api-data", JSON.stringify(apiData));
+    console.log("API called, data stored", apiData);
+  },
 
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// Endpoint to return last API data
-		if (new URL(request.url).pathname === "/last-api-data") {
-			const data = await env.API_DATA.get('last-api-data');
-			if (!data) {
-				return new Response('No Data', {
-					headers: { "Content-Type": "application/json" },
-				});
-			}
-			return new Response(data, {
-				headers: { "Content-Type": "application/json" },
-			});
-		}
-		return new Response("Not found", { status: 404 });
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    // Endpoint to return last API data
+    if (new URL(request.url).pathname === "/last-api-data") {
+      const data = await env.API_DATA.get("last-api-data");
+      if (!data) {
+        return new Response("No Data", {
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+      return new Response(data, {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    return new Response("Not found", { status: 404 });
+  },
 };
